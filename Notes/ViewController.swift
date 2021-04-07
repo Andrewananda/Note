e1065b9109e00b05bf1ebbac9acf2682ee1480f3
andrew.tiema <andrew.tiema@ajiraconnect.com>
2021-04-07 18:59:46 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Andrew Ananda on 07/04/2021.
+//  Copyright © 2021 Andrew Ananda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
